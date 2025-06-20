0185171cc9372a47e4b8c70773e5d6492c7370d6
lee-dohyun <leedohyun1985@gmail.com>
2025-06-20 15:14:26 +0900

    CrossOrigin 허용

--- src/main/java/com/dh/auth/controller/JwtController.java
+++ src/main/java/com/dh/auth/controller/JwtController.java
@@ -1,10 +1,12 @@
 package com.dh.auth.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "https://*.leedohyun.com")
 @RestController
 public class JwtController {
 
