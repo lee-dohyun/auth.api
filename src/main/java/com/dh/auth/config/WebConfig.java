559b24d12fb622893fd86942f04381f6a2f87265
lee-dohyun <leedohyun1985@gmail.com>
2025-06-20 16:08:59 +0900

    cors 설정 추가

---
+++ src/main/java/com/dh/auth/config/WebConfig.java
@@ -0,0 +1,20 @@
+package com.dh.auth.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig {
+	@Bean
+	WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOriginPatterns("https://*.leedohyun.com")
+						.allowedMethods("GET", "POST", "OPTIONS").allowCredentials(true);
+			}
+		};
+	}
+}
